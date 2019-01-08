069f73ecb07f8e238dc91b6b54a0d113b9a6d0d4
闫涛 <554306103@qq.com>
2019-01-08 16:11:44 +0800

    用户管理

---
+++ application/admin/model/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class User extends Model
+{
+    public function getUser()
+    {
+        return $this->field('*')
+            ->where('admin','eq',1)
+            ->select();
+    }
+
+    public function addAdmin($args)
+    {
+        return $this->save($args);
+    }
+
+//    protected $autoWriteTimestamp = true;
+//    protected $createTime = 'createtime';
+//    protected $updateTime = 'updatetime';
+}
